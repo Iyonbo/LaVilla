6ef72272d3c1ce68e8f6ee8f87deead0f0a3a1d2
Luis Chavarria <luisfer.chavarria@gmail.com>
2021-10-23 01:13:30 -0600

    Change

--- wp-content/themes/vw-interior-designs/template-parts/content-calculadora.php
+++ wp-content/themes/vw-interior-designs/template-parts/content-calculadora.php
@@ -13,11 +13,24 @@
     <?php the_post_thumbnail(); ?>
     <hr>
   <?php }?>
-  <h1 class="vw-page-title"><?php the_title();?></h1>
-  <div class="entry-content"><?php the_content();?></div>
-  <?php
-      // If comments are open or we have at least one comment, load up the comment template.
-     echo "Hola mundo"
-  ?>
+    <h1 class="vw-page-title"><?php the_title();?></h1>
+    <div class="entry-content">
+      <?php the_content();?>
+      <form action="">
+          <label>Tipo de cambio USD 1.00 = GTQ 7.00</label>
+          <label for="">Tipo de conversión
+              <select name="tipo" id="">
+                  <option value="1"> Dolares a Quetzales</option>
+                  <option value="2"> Quetzales a Dolares</option>
+              </select>
+          </label>
+          <label for="">Cantidad
+              <input type="number" name="cantidad">
+          </label>
+          <label for="">Resultado
+              <span>323</span>
+          </label>
+      </form>
+    </div>
   <div class="clearfix"></div>
 </div>
